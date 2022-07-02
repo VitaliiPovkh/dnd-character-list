2d3269b310e3fd8765602f34809bc21771b53718
UmbraSage <zerkishmark@gmail.com>
2022-07-02 15:15:32 +0300

    Added sex choose, improved calculation of damage? and hp system. fixed bugs on STATS window

--- DnD_CharacterList/CharacterDataClasses/Character.cs
+++ DnD_CharacterList/CharacterDataClasses/Character.cs
@@ -6,7 +6,119 @@ using System.Threading.Tasks;
 
 namespace DnD_CharacterList.CharacterDataClasses
 {
+    public enum Race
+    {
+        HUMAN, ELF, HALF_ELF, GNOME, DWARF,
+        DRAGONBORN, CHANGELING, ORC, HALF_ORC,
+        SATYR, TABAXI, TIEFLING 
+    }
+
+    public enum Class
+    {
+        BARD, BARBARIAN, WARRIOR, WIZARD, DRUID,
+        PRIEST, INVENTOR, WARLOCK, MONK, PALADIN, 
+        DODGER, PATHFINDER, SORCERER
+    }
+
     public class Character
     {
+        /*
+         * TODO:
+         * Relocate non-input data calculations from form classes to character classes
+         * Example: level calculations, depending from expirience value
+         * or
+         * ArmorClass, depending from character`s equipment
+         */
+        private int currentHP;
+
+        public string? Name { get; set; }
+        public string? Race { get; set; } // Enum ??
+        public string? Class { get; set; } // Enum ??
+        public string? Sex { get; set; }
+        public int MaxHP { get; private set; }
+        public int CurrentHP
+        {
+            get { return currentHP; }
+            set 
+            {
+                if (value > MaxHP)
+                {
+                    currentHP = MaxHP;
+                }
+                else
+                {
+                    currentHP = value;
+                }
+            }
+        }
+        public int Expirience { get; set; }
+        public int Level { get; private set; }
+        public int ArmourClass { get; private set; } //?
+        public int Initiative { get; set;}
+        public bool[] LifeThrows { get; private set; }
+        public bool[] DeathThrows { get; private set; }
+        public Personality CharactersPersonality { get; private set; }
+        public Equipment CharactersEquipment { get; private set; }
+        public Inventory CharactersInventory { get; private set; }
+        public Stats CharactersStats { get; private set; }
+        public Ownership CharactersOwnership { get; private set; }
+        public Notes CharactersNotes { get; private set; }
+        public Spells CharactersSpells { get; private set; }
+
+
+        private readonly List<int> expirienceMilestones;
+
+        public Character()
+        {
+            MaxHP = 28; //temp
+           CurrentHP = MaxHP;//temp
+
+            LifeThrows = new bool[3];
+            DeathThrows = new bool[3];
+
+            CharactersPersonality = new Personality();
+            CharactersEquipment = new Equipment();
+            CharactersInventory = new Inventory();
+            CharactersStats = new Stats();
+            CharactersOwnership = new Ownership();
+            CharactersNotes = new Notes();
+            CharactersSpells = new Spells();
+
+            expirienceMilestones = new List<int>()
+            {
+                300, 900, 2700, 6500, 14000, 23000,
+                34000, 48000, 64000, 85000, 100000,
+                120000, 140000, 165000, 195000, 225000,
+                265000, 305000, 355000
+            };
+        }
+        
+        public void RecalculateLevel()
+        {
+            int biggerMilestone = expirienceMilestones.Find(x =>
+            {
+                int lastMilestone = expirienceMilestones[expirienceMilestones.Count - 1];
+                return Expirience < x || (x == lastMilestone && Expirience >= lastMilestone);
+            });
+
+            int level = expirienceMilestones.IndexOf(biggerMilestone) + 1;
+            if (Expirience >= expirienceMilestones[level - 1])
+            {
+                level++;
+            }
+            Level = level;
+        }
+        public void HpDamageHeal(int value)
+        {
+            try
+            {
+               CurrentHP = CurrentHP + value;
+                if (CurrentHP < 0) { CurrentHP = 0;}
+                if (CurrentHP > MaxHP) { CurrentHP = MaxHP; }
+                
+            }
+            catch { }
+
+        }
     }
 }
--- DnD_CharacterList/MainWindow.xaml.cs
+++ DnD_CharacterList/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
 using DnD_CharacterList.CharacterDataClasses;
 
@@ -9,104 +11,69 @@ namespace DnD_CharacterList
     /// </summary>
     public partial class MainWindow : Window, ICharacterWindow
     {
-        private string[] CharRace;
-        private string[] CharClass;
+        private string[] charRace;
+        private string[] charClass;
+        private double healthbar;
+   
         public Character CharacterData { get; set; }
+
         public MainWindow()
-        {   InitializeComponent();
+        {
+            InitializeComponent();
+
             RaceBox.SelectionChanged += RaceBox_SelectionChanged;
             ClassBox.SelectionChanged += ClassBox_SelectionChanged_1;
             XpBox.TextChanged += XpBox_TextChanged;
-            CharRace = new string[] {"Human","Elf","Half-Elf","Gnome","Dwarf","Dragonborn","Changeling",
-            "Ork","Half-Ork","Satyr","Tabaxy","Tiefling" };
-            CharClass = new string[] { "Bard", "Barbarian", "Warrior", "Wizard", "Druid", "Priest",
-                "Inventor","Warlock", "Monk","Paladin","Dodger", "Pathfinder","Corcerer"};
+
+            charRace = new string[]
+            {
+                "Human", "Elf", "Half-Elf", "Gnome", "Dwarf",
+                "Dragonborn", "Changeling", "Ork", "Half-Ork",
+                "Satyr", "Tabaxy", "Tiefling"
+            };
+
+            charClass = new string[]
+            {
+                "Bard", "Barbarian", "Warrior", "Wizard", "Druid",
+                "Priest","Inventor", "Warlock", "Monk", "Paladin",
+                "Dodger", "Pathfinder", "Corcerer"
+            };
             CharacterData = new Character();
+            MaxHealth.Content = CharacterData.MaxHP;
+            CurrentHealth.Text = CharacterData.CurrentHP.ToString();
         }
-       
-        
-        private void RaceBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+
+
+        private void DamageHealBar_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {    if (e.Key == System.Windows.Input.Key.Enter)
+             {  CharacterData.HpDamageHeal(int.Parse(DamageHealBar.Text));
+                CurrentHealth.Text = CharacterData.CurrentHP.ToString();
+                healthbar = (double)CharacterData.CurrentHP / CharacterData.MaxHP * 100;
+                HealthBar.Value = (int)healthbar;
+                DamageHealBar.Text = "";
+             }
+        }
+        private void DamageHealBar_TextChanged(object sender, TextChangedEventArgs e)
         {
            
         }
-
+        private void RaceBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CharacterData.Race = charRace[RaceBox.SelectedIndex];
+           
+           
+        }
         private void ClassBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
-            
+            CharacterData.Class = charClass[ClassBox.SelectedIndex];
         }
-       
-        
         private void XpBox_TextChanged(object sender, RoutedEventArgs e) {
             int xpcount = 0;
-            try{xpcount = int.Parse(XpBox.Text);} 
-            catch {}
-            
-            switch (xpcount) 
-            {
-                case <300: LevelCount.Content = "1";
-                    break;
-                case < 900:
-                    LevelCount.Content = "2";
-                    break;
-                case < 2700:
-                    LevelCount.Content = "3";
-                    break;
-                case < 6500:
-                    LevelCount.Content = "4";
-                    break;
-                case < 14000:
-                    LevelCount.Content = "5";
-                    break;
-                case < 23000:
-                    LevelCount.Content = "6";
-                    break;
-                case < 34000:
-                    LevelCount.Content = "7";
-                    break;
-                case < 48000:
-                    LevelCount.Content = "8";
-                    break;
-                case < 64000:
-                    LevelCount.Content = "9";
-                    break;
-                case < 85000:
-                    LevelCount.Content = "10";
-                    break;
-                case < 100000:
-                    LevelCount.Content = "11";
-                    break;
-                case < 120000:
-                    LevelCount.Content = "12";
-                    break;
-                case < 140000:
-                    LevelCount.Content = "13";
-                    break;
-                case < 165000:
-                    LevelCount.Content = "14";
-                    break;
-                case < 195000:
-                    LevelCount.Content = "15";
-                    break;
-                case < 225000:
-                    LevelCount.Content = "16";
-                    break;
-                case < 265000:
-                    LevelCount.Content = "17";
-                    break;
-                case < 305000:
-                    LevelCount.Content = "18";
-                    break;
-                case < 355000:
-                    LevelCount.Content = "19";
-                    break;
-                case >=355000:
-                    LevelCount.Content = "20";
-                    break;
+            int.TryParse(XpBox.Text, out xpcount);
 
-            }
-
-
-
+            CharacterData.Expirience = xpcount;
+            CharacterData.RecalculateLevel();
+            LevelCount.Content = CharacterData.Level; 
         }
         private void equipment_ButtonClick(object sender, RoutedEventArgs e)
         {   
@@ -153,6 +120,13 @@ namespace DnD_CharacterList
             ThrowDicesWindow dicesWindow = new ThrowDicesWindow(CharacterData);
             dicesWindow.Show();
         }
-
+        private void SEXM_Checked(object sender, RoutedEventArgs e)
+        {
+            CharacterData.Sex = "Male";
+        }
+        private void SEXW_Checked(object sender, RoutedEventArgs e)
+        {
+            CharacterData.Sex = "Female";
+        }
     }
 }
