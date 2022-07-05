5701c0ff67e551498adcb6fc1173c72f179c0766
VitaliiPovkh <fehold2000@gmail.com>
2022-07-05 18:12:32 +0200

    Started weapon system implementation

--- DnD_CharacterList/CharacterDataClasses/EquipmentTypes/Weapons/Weapon.cs
+++ DnD_CharacterList/CharacterDataClasses/EquipmentTypes/Weapons/Weapon.cs
@@ -1,14 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 
 namespace DnD_CharacterList.CharacterDataClasses.EquipmentTypes.Weapons
 {
-    public enum WeaponType
-    {
 
+    public enum DamageType
+    {
+        PIERCING,
+        SLASHING,
+        BLUDGEONING,
+        ACID,
+        COLD,
+        FIRE,
+        FORCE,
+        LIGHTNING,
+        NECROTIC,
+        POISON,
+        PSYCHIC,
+        RADIANT,
+        THUNDER
+    }
+
+    public enum WeaponMaterial
+    {
+        STANDART,
+        SILVER,
+        ADAMANT
+    }
+
+    public enum HandsRequirement
+    {
+        ONEHANDED,
+        DOUBLEHANDED,
+        UNIVERSAL
+    }
+
+    public enum WeaponSize
+    {
+        SMALL,
+        AVARAGE,
+        LARGE
     }
 
     public class Weapon
@@ -18,6 +49,11 @@ namespace DnD_CharacterList.CharacterDataClasses.EquipmentTypes.Weapons
         public List<Dice> DamageDices { get; private set; }
         public bool Hindrance { get; set; }
         public bool Advantage { get; set; }
+        public DamageType DamageType { get; set; }
+        public WeaponMaterial WeaponMaterial { get; set; }
+        public HandsRequirement HandsRequirement { get; set; }
+        public WeaponSize WeaponSize { get; set; }
+
 
         public Weapon()
         {
